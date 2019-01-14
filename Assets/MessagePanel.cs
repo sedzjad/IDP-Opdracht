92c27edd5bca01a18c79d83f0e55c35b100da618
sedzjad <stsheefe@gmail.com>
2019-01-14 09:06:34 +0100

    Adding the area and player

---
+++ Assets/MessagePanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessagePanel : MonoBehaviour
+{
+
+    public GameObject MessageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMessageText(string text)
+    {
+        MessageText.SetActive(true);
+    }
+
+    public void CloseMessageText()
+    {
+        MessageText.SetActive(false);
+    }
+}
